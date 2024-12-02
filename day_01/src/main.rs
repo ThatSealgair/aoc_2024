b656b154454dc6502555fdcaaff7fb34a51d546c
Sealgair <hunter.jay.k@gmail.com>
2024-12-02 20:03:08 +1000

    Changed approach to problem.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,27 +1,16 @@
 use std::fs::File;
 use std::io::env;
+use std::io::read_to_string;
 use std::io::Result;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-#[derive(Debug)]
-enum Error {
-    IoError(std::io::Error),
-    NoFilename,
-}
-
-impl From<std::io::Error> for Error {
-    fn from(error: std::io::Error) -> Self {
-        Error::IoError(error)
-    }
-}
-
 fn main() {
-    let filename = env::args().nth(1).ok_or(Error::NoFilename)?;
-    let path = Path::new(&filename);
-    let (left_set, right_set) = process_two_columns(path)?;
+    let filename = env::args().nth(1).ok_or("Usage: day_one [file_name]")?;
+    let input = read_to_string(filename)?;
+    let result = process_input_stage_one(&input);
 
-    println!("Part one: TODO");
+    println!("Part one: {:?}", result);
     println!("Part two: TODO");
     Ok(());
 }
