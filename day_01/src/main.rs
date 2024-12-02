61410c9d2c1350fcc49fca42189330dd9e0493ab
Sealgair <hunter.jay.k@gmail.com>
2024-12-02 20:23:38 +1000

    Updated imports to reflect new approach. Finished function logic for parse_input(). Removed minor bugs and typos.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,9 +1,5 @@
-use std::fs::File;
-use std::io::env;
-use std::io::read_to_string;
-use std::io::Result;
-use std::io::{BufRead, BufReader};
-use std::path::Path;
+use std::env;
+use std::error::Error;
 
 fn process_input_stage_one(input: &str) -> Result<i32, Box<dyn Error>> {
     let (left, right) = parse_input(input)?;
@@ -21,12 +17,22 @@ fn parse_input(input: &str) -> Result<(Vec<i32>, Vec<i32>), Box<dyn Error>> {
             return Err("Expected each line to contain exactly two numbers".into());
         }
 
-        Ok((numbers[0], number[1]))
+        Ok((numbers[0], numbers[1]))
     };
+
+    let (left, right): (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(parse_line)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .unzip();
+
+    Ok((left, right))
 }
 
 fn main() {
-    let filename = env::args().nth(1).ok_or("Usage: day_one [file_name]")?;
+    let filename = env::args().nth(1).ok_or("Usage: day_one [file_name]");
     let input = read_to_string(filename)?;
     let result = process_input_stage_one(&input);
 
