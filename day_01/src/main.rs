7d428c17ece8cd4681a00df1a7eb1b5f3faf5620
Sealgair <hunter.jay.k@gmail.com>
2024-12-02 20:04:56 +1000

    Added outline for processing stage one input

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -5,6 +5,11 @@ use std::io::Result;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+fn process_input_stage_one(input: &str) -> Result<i32, Box<dyn Error>> {
+    let (left, right) = parse_input(input)?;
+    Ok(total_distance(left, right))
+}
+
 fn main() {
     let filename = env::args().nth(1).ok_or("Usage: day_one [file_name]")?;
     let input = read_to_string(filename)?;
